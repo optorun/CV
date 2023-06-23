a35b4f3e56163e4240b3cc614b5e30c32a927840
ptn <pierre@touron.eu>
2023-06-23 13:56:23 +0200

    MOD entrypoint.sh -> templates/entrypoint.sh.j2 / On passe l'entrypoint dans un template pour pouvoir le modifier simplement depuis le playbook; MOD templates/Dockerfile.j2 / On adapte le Dockerfile pour Ubuntu, puisque la variante alpine de l'image de base ne permet pas simplement d'installer wkhtmltopdf, et que je n'ai pas particulièrement envie d'ajouter des steps pour le build depuis les sources; MOD cv_build.yml / Modif du tag de l'image pour utiliser la variante Ubuntu + gestion du template de l'entrypoint + ajout d'une option pour force le rebuild de l'image; MOD templates/Makefile.j2 / Suppression des targets inutiles + modif de la target pdf pour utiliser wkhtmltopdf en direct; reste quelques ajustements sur le style et peut-être la commande, ainsi que le workflow (obligé d'appeler la target html avant si le fichier n'est pas déjà présent; on peut faire plus propre)

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#sh -c "cd /home/app/resume && make html && make pdf" # On ne build pas en pdf pour l'instant puisque wkhtmltopdf ne fonctionne pas correctement avec pandoc
-sh -c "cd /home/app/resume && make html"
